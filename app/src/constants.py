041327eba49109b9a44fa67bba99bd9dbad35bfa
MRossa <maxim.rossa@mail.ru>
2024-06-30 20:20:27 +0400

    Removed extra constant

--- app/src/constants.py
+++ app/src/constants.py
@@ -1,6 +1,4 @@
 # all path are from ./src folder
-DATASET_PATH = r"dataset/mart_export.txt"
-
-DATASET_PATH_SANDBOX = r"../dataset/mart_export.txt"
+DATASET_PATH = r"../dataset/mart_export.txt"
 
 NUCLEOTIDE_MAPPING = {"A": 0, "C": 1, "G": 2, "T": 3}
