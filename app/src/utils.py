4a13ac66d3b5949e45f06f2198a95eed0cd84463
MRossa <maxim.rossa@mail.ru>
2024-06-30 20:19:02 +0400

    Remove one-hot encoder, all logic has been moved to the model class

--- app/src/utils.py
+++ app/src/utils.py
@@ -1,7 +1,5 @@
 import logging
 
-import numpy as np
-from constants import NUCLEOTIDE_MAPPING
 from tqdm import tqdm
 
 
@@ -27,39 +25,5 @@ def generate_dataset(sequences, window_size=15):
                 y.append(seq[i])
         else:
             logging.debug(f"Skip sequence {seq} cause too small (<{threshold})")
+
     return X_left, X_right, y
-
-
-def one_hot_encode(seq):
-    new_seq = []
-    for nucleotide in tqdm(seq, desc="One hot encoding"):
-        encoded_chain = ""
-        if len(nucleotide) == 1:
-            encoded_chain += str(NUCLEOTIDE_MAPPING[nucleotide])
-        else:
-            for char_nucleotide in nucleotide:
-                encoded_chain += str(NUCLEOTIDE_MAPPING[char_nucleotide])
-        new_seq.append(encoded_chain)
-
-    return new_seq
-
-
-if __name__ == "__main__":
-    X_left = [
-        "ATGGAGATCCCTGTG",
-        "TGGAGATCCCTGTGC",
-        "GGAGATCCCTGTGCC",
-        "GAGATCCCTGTGCCT",
-        "AGATCCCTGTGCCTG",
-        "GATCCCTGTGCCTGT",
-        "ATCCCTGTGCCTGTG",
-        "TCCCTGTGCCTGTGC",
-    ]
-
-    y = ["C", "C", "T", "G", "T", "G", "C", "A", "G", "C", "C", "G", "T", "C", "T"]
-
-    new_X = one_hot_encode(X_left)
-
-    new_Y = one_hot_encode(y)
-
-    print(new_X, new_Y)
